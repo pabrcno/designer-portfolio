51848c64c5669f76ac28d725c888b745055ca5e9
pabrcno <pabrcn@gmail.com>
2021-09-06 18:52:34 -0300

    App.js cleaning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import "./App.css";
-
-import StartingSection from "./components/StartingSection/StartingSection";
 import MainScreen from "./screens/Main";
 function App() {
   return <MainScreen />;
